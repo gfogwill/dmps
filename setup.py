3f615e91af6d20db6134f484409482fc82d7f801
gfogwill <gfogwill@smn.gov.ar>
2021-06-07 19:45:37 -0300

    Initial import

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Interface with DMPS observations',
+    author='gfogwill',
+    license='MIT',
+)

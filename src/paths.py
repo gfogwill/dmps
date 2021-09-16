61f7ea28fe057ee1fd754c520d739562e45cf055
gfogwill <gfogwill@smn.gov.ar>
2021-09-16 20:39:16 -0300

    feat: create dmps cli

--- src/paths.py
+++ src/paths.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 
-# Get the project directory as the parent of this module location
+# Get the project directory as the parent of this module locations
 src_module_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 project_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent
 

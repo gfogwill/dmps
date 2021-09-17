311e328d47e292b27ec2c5a9db9986df15d75915
gfogwill <gfogwill@smn.gov.ar>
2021-09-16 21:35:49 -0300

    feat: create test function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,13 +6,10 @@ from click.testing import CliRunner
 class TestCLI(unittest.TestCase):
     def test_cli(self):
         """Run `dmps` without arguments."""
-        result = CliRunner().invoke(cli, [])
+        result = CliRunner().invoke(dmps, [])
 
         assert result.exit_code == 0
         assert "dmps" in result.output
-
-    def test_version(self):
-        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
